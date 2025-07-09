f58dddc7e781d424ca3accc35e8fb848d9c7dd8f
Muxtar Abdullayev <nice.biz@bk.ru>
2025-07-09 17:50:50 +0300

    register ve login controller yazildi.

--- src/main/java/com/example/bankdb/controller/AuthController.java
+++ src/main/java/com/example/bankdb/controller/AuthController.java
@@ -1,4 +1,28 @@
 package com.example.bankdb.controller;
 
+import com.example.bankdb.model.dto.LoginRequest;
+import com.example.bankdb.model.dto.RegisterRequest;
+import com.example.bankdb.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserService userService;
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+        return userService.register(request);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+        return userService.login(request);
+    }
 }
